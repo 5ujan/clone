edbc1e54f13b67ede9e5ccf93f65b3bc137353c6
5ujan <60572186+5ujan@users.noreply.github.com>
2023-01-17 21:54:25 +0545

    Add files via upload

---
+++ Person.js
@@ -0,0 +1,32 @@
+import React, {useState,createRef} from 'react'
+import { FiMail } from "react-icons/fi";
+import { FaLinkedin } from "react-icons/fa";
+
+
+const Overlay=()=>{
+    return(
+
+        <div className='person-overlay'>
+            <h3 className='person-name'>Random Person</h3>
+            <h4 className='person-role'>Person role</h4>
+            <span className='contact'>{Math.random()>0.5?<FaLinkedin></FaLinkedin>:null} <FiMail></FiMail></span>
+        </div>
+        )
+}
+const Person=()=>{
+    const personContainer = createRef()
+    // const [hover, setHover] = useState(false)
+    return (
+      <article
+        className="person"
+        ref={personContainer}
+        >
+        <Overlay ></Overlay>
+        <div className="person-wrapper" ref={personContainer}>
+          <img className="person-img" src={"/assets/gm.jpg"} alt="" />
+        </div>
+      </article>
+    );
+
+}
+export default Person
